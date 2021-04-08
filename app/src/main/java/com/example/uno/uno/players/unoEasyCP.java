34732ab2ad24542065d0f15531c25d55f883d72c
chiara <profenna23@up.edu>
2021-04-08 15:35:14 -0700

    added pseudocode & comments to CP classes

--- app/src/main/java/com/example/uno/uno/players/unoEasyCP.java
+++ app/src/main/java/com/example/uno/uno/players/unoEasyCP.java
@@ -1,7 +1,10 @@
 package com.example.uno.uno.players;
 
+import com.example.uno.Card;
 import com.example.uno.game.GameFramework.infoMessage.GameInfo;
 import com.example.uno.game.GameFramework.players.GameComputerPlayer;
+
+import java.util.Random;
 
 public class unoEasyCP extends GameComputerPlayer {
 
@@ -26,6 +29,48 @@ public class unoEasyCP extends GameComputerPlayer {
         // randomly pick card from hand
         // take game, send new playcard action
 
+        /**
+         * Implementation:
+         * - read the card on top of discard pile
+         * - make a new list of all playable cards
+         * - if list is empty:
+         *      - draw a card from the draw pile
+         * - else:
+         *      - create a new random that will pick a number from 0-size of playable cards list
+         *      - Play the card that corresponds to the number
+         */
+
+        // 1. verify gameinfo is a gamestate obj
+        if (info != gamestate obj){
+            // if not, exit - else, continue
+            exit;
+        }
+
+        // 2. read card on top of discard pile
+        // find last item in discardPile
+        Card lastCardPlayed = // insert last item in discardPile
+
+        // 3. make a list of playable cards
+        ArrayList <Card> playableCards;
+
+        // 4. go through players card & add to arraylist
+        // use an array of players cards
+        // find number of cards in hand
+        int numCards;
+        for (int i=0; i<numCards; i++){
+            // if color or number of cardsInHandP1[i] matches lastCardPlayed
+                // add to playableCards list
+            // else
+                // continue
+        }
+
+        // choose random card from playableCards
+        Random rndCard = new Random();
+        int number = rndCard.nextInt(sizeof(playableCards));
+        Card cardToPlay = playableCards[number];
+
+
+        // sends action - last line
         game.sendAction(new unoPlayCard(cardToPlay));
 
     }
--- app/src/main/java/com/example/uno/uno/players/unoHardCP.java
+++ app/src/main/java/com/example/uno/uno/players/unoHardCP.java
@@ -21,6 +21,24 @@ public class unoHardCP extends GameComputerPlayer {
      */
     @Override
     protected void receiveInfo(GameInfo info) {
+        /**
+         * Implementation:
+         * - read the card on top of discard pile
+         * - make a new list of all playable cards
+         * - if list is empty:
+         *      - draw a card from the draw pile
+         * - else:
+         *      - if there is a special card, use that first following this order:
+         *          - Draw 2
+         *          - Skip
+         *          - Reverse
+         *      - if there are no special cards, try a wild in this order:
+         *          - Wild draw 4
+         *          - Wild regular color change
+         *      - if there are no special or wild cards, use a face card
+         */
 
+        // sends action - last line
+        game.sendAction(new unoPlayCard(cardToPlay));
     }
 }
