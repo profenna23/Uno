2ca907b1c191a8b0f51b5b7d9f54414634efb700
nicholso24 <nicholso24@up.edu>
2021-04-08 17:35:46 -0700

    Added most of code to UnoLocalGame

--- app/src/main/java/com/example/uno/UnoLocalGame.java
+++ app/src/main/java/com/example/uno/UnoLocalGame.java
@@ -34,11 +34,21 @@ public class UnoLocalGame extends LocalGame {
 
         UnoState state = (UnoState) super.state;
 
-        for(int i = 0; i < 4; i++) {
-
+        if(state.getCardsInHandP1().size() == 0 && state.getCardsInHandP1().get(0) != null ) {
+            return "Player 1 has won!";
+        }
+        if(state.getCardsInHandP2().size() == 0 && state.getCardsInHandP2().get(0) != null) {
+            return "Player 2 has won!";
+        }
+        if(state.getCardsInHandP3().size() == 0 && state.getCardsInHandP3().get(0) != null) {
+            return "Player 2 has won!";
+        }
+        if(state.getCardsInHandP4().size() == 0 && state.getCardsInHandP4().get(0) != null) {
+            return "Player 2 has won!";
         }
 
-        return null;
+
+
     }
 
     @Override
