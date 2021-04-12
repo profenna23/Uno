2baa7a5dc294976389eda7494fa833d1df2296f1
Meganly14 <lym24@up.edu>
2021-04-12 15:30:55 -0700

    making updates

--- app/src/main/java/com/example/uno/uno/tttActionMessage/actions/unoPlayCard.java
+++ app/src/main/java/com/example/uno/uno/tttActionMessage/actions/unoPlayCard.java
@@ -1,11 +1,14 @@
 package com.example.uno.uno.tttActionMessage.actions;
 
 import com.example.uno.Card;
+import com.example.uno.UnoLocalGame;
+import com.example.uno.UnoState;
 import com.example.uno.game.GameFramework.actionMessage.GameAction;
 import com.example.uno.game.GameFramework.players.GamePlayer;
 
-public class unoPlayCard extends GameAction {
-    private final
+public class unoPlayCard implements UnoLocalGame, UnoState {
+    private int playerID;
+    private final cardPlayed;
 
     /**
      * constructor for GameAction
@@ -13,16 +16,16 @@ public class unoPlayCard extends GameAction {
      * @param player the player who created the action
      */
     public unoPlayCard(GamePlayer player, Card cardToPlay) {
-
+        private int playerID;
+        private final cardPlayed;
         //2 getters
         //player - who's playing,
         //onclick sends that command -- how do i implement this
         //local game will use the info to set the game
-        super(player);
+
 
 
     }
-
 }
 
 
