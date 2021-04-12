b908e6a1a94d0e60b75578aec76a0be095815e8f
Meganly14 <lym24@up.edu>
2021-04-12 14:52:19 -0700

    classes updated again

--- app/src/main/java/com/example/uno/uno/tttActionMessage/actions/unoDrawCard.java
+++ app/src/main/java/com/example/uno/uno/tttActionMessage/actions/unoDrawCard.java
@@ -5,25 +5,21 @@ import com.example.uno.game.GameFramework.infoMessage.GameState;
 import com.example.uno.game.GameFramework.players.GamePlayer;
 
 public class unoDrawCard extends GameAction {
-    public int player;
-    public int getPlayerTurn(){
-        return player;
-    }
+    //public int player;
+    //public int getPlayerTurn(){
+      //  return player;
+    //}
     /**
      * constructor for GameAction
      *
      * @param player the player who created the action
      */
-    public unoDrawCard(GamePlayer player, GameState drawCard) {
+    public unoDrawCard(GamePlayer player) {
         //drawcard method in gamestate
         //which player is requesting, and this is who they are
 
         super(player);
-        if(getPlayerTurn() == 1){
 
-        }
-        if(getPlayerTurn() == 2){
 
-        }
     }
 }
