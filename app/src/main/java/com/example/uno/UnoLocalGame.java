9bac04313bf850d6458477a971c57034d09daa50
nicholso24 <nicholso24@up.edu>
2021-04-29 13:54:34 -0700

    Can be played to Victory

--- app/src/main/java/com/example/uno/UnoLocalGame.java
+++ app/src/main/java/com/example/uno/UnoLocalGame.java
@@ -42,17 +42,17 @@ public class UnoLocalGame extends LocalGame {
 
         UnoState state = (UnoState) super.state;
 
-        if(state.getCardsInHandP1().size() == 0 && state.getCardsInHandP1().get(0) != null ) {
+        if(state.getCardsInHandP1().size() == 0) { //&& state.getCardsInHandP1().get(0) != null
             return "Player 1 has won!";
         }
-        if(state.getCardsInHandP2().size() == 0 && state.getCardsInHandP2().get(0) != null) {
+        if(state.getCardsInHandP2().size() == 0) {
             return "Player 2 has won!";
         }
-        if(state.getCardsInHandP3().size() == 0 && state.getCardsInHandP3().get(0) != null) {
-            return "Player 2 has won!";
+        if(state.getCardsInHandP3().size() == 0) {
+            return "Player 3 has won!";
         }
-        if(state.getCardsInHandP4().size() == 0 && state.getCardsInHandP4().get(0) != null) {
-            return "Player 2 has won!";
+        if(state.getCardsInHandP4().size() == 0) {
+            return "Player 4 has won!";
         }
         return null;
 
